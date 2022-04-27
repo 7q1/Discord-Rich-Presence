97969f7ccb6eb02ada7cd4ef8c56d504ab2a99db
Bader <104280578+7q1@users.noreply.github.com>
2022-04-27 17:50:23 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+'''
+
+@github.com/7q1
+
+'''
+
+appId = "968820014801100823" # Input your Application ID here
+
+details     = ""	# Details.
+state       = ""	# State.
+large_image = ""	# Large image <name> you put in your application id.
+small_image = ""	# Small image <name> you put in your application id.
+large_text  = ""	# Large image hover text.
+
+# My Configs In Picture.:
+
+# details = "def isCoding():"
+# state = "‎\treturn"
+# large_image = "python"    
+# small_image = "  "	      
+# large_text = ".py"		   
---
+++ rich.py
@@ -0,0 +1,51 @@
+import config
+from config import *
+from pypresence import Presence as pre
+from colorama import *
+import time
+
+BA = ("""
+
+╔══════╗  ╔════════╗      ╔═╗
+╚════╗ ║  ║ ╔════╗ ║     ╔  ║
+    ╔╝╔╝  ║ ║ ╔╗ ║ ║   ╔═╝  ║
+   ╔╝╔╝   ╚╗  ╔╗  ╔╝   ╚═╝  ║  
+  ╔╝╔╝     ╚═╗  ╔═╝         ╚═╗
+  ╚═╝        ╚══╝     ╚══════╝\n""")
+print("Discrd Rich Presence By:\n"); print(f"{Fore.BLUE}{BA}{Fore.RESET}")
+appId = config.appId 
+richPresence = pre(appId) 
+richPresence.connect()
+
+# <!------ Config ------ >
+details = config.details
+state = config.state
+large_image = config.large_image
+small_image = config.small_image
+large_text = config.large_text
+# <!------ /Config ------ >
+
+# Display .
+print(F'''
+[{Fore.YELLOW}>{Fore.RESET}] Details     {Fore.RED}->{Fore.RESET} [{details}]
+[{Fore.YELLOW}>{Fore.RESET}] State       {Fore.RED}->{Fore.RESET} [{state}]
+[{Fore.YELLOW}>{Fore.RESET}] Large_image {Fore.RED}->{Fore.RESET} [{large_image}]
+[{Fore.YELLOW}>{Fore.RESET}] Small_image {Fore.RED}->{Fore.RESET} [{small_image}]
+[{Fore.YELLOW}>{Fore.RESET}] Large_text  {Fore.RED}->{Fore.RESET} [{large_text}]
+''')
+
+# Updating Rich Presence.
+richPresence.update(details=details,
+            state=state,
+            large_image=large_image,
+            small_image=small_image,
+            large_text=large_text,
+            start=time.time())
+
+print(F"\n[{Fore.GREEN}+{Fore.RESET}] Rich Presence Running.")
+
+# Looping time sleep so the script keep on running.
+while 1:
+    time.sleep(1)
+
+# <!-- Null -->
