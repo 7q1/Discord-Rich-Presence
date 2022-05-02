a8f5745c9bc9dc0cf590d22885b500300ad8361b
Bader <104280578+7q1@users.noreply.github.com>
2022-05-02 18:08:29 +0300

    Update rich.py

--- rich.py
+++ rich.py
@@ -13,49 +13,67 @@ from pypresence import Presence as pre
 from colorama import *
 import time
 os.system("cls")
+# HyperLink
+def myLink(url, label=None):
+    if label is None: 
+        label = url
+    parameters = ''
 
-BA = ("""
+    # OSC 8 ; params ; URI ST <name> OSC 8 ;; ST 
+    escape_mask = '\033]8;{};{}\033\\{}\033]8;;\033\\'
+
+    return escape_mask.format(parameters, url, label)
+
+BA = (F"""{Fore.CYAN}
 
 ╔══════╗  ╔════════╗      ╔═╗
 ╚════╗ ║  ║ ╔════╗ ║     ╔  ║
     ╔╝╔╝  ║ ║ ╔╗ ║ ║   ╔═╝  ║
    ╔╝╔╝   ╚╗  ╔╗  ╔╝   ╚═╝  ║  
   ╔╝╔╝     ╚═╗  ╔═╝         ╚═╗
-  ╚═╝        ╚══╝     ╚══════╝\n""")
-print("Discrd Rich Presence By:\n"); print(f"{Fore.BLUE}{BA}{Fore.RESET}")
-appId = config.appId 
-richPresence = pre(appId) 
-richPresence.connect()
+  ╚═╝        ╚══╝     ╚══════╝{Fore.RESET}  {Fore.MAGENTA}{myLink("https://github.com/7q1", "GitHub.")}{Fore.RESET}\n""")
 
-# <!------ Config ------ >
-details = config.details
-state = config.state
-large_image = config.large_image
-small_image = config.small_image
-large_text = config.large_text
-# <!------ /Config ------ >
+while 1:
+    try:
+        appId = config.appId
+        richPresence = pre(appId) 
+        richPresence.connect()
+        os.system('cls||clear')
+        print("Discrd Rich Presence By:\n"); print(BA)
 
-# Display .
-print(F'''
-[{Fore.YELLOW}>{Fore.RESET}] Details     {Fore.RED}->{Fore.RESET} [{details}]
-[{Fore.YELLOW}>{Fore.RESET}] State       {Fore.RED}->{Fore.RESET} [{state}]
-[{Fore.YELLOW}>{Fore.RESET}] Large_image {Fore.RED}->{Fore.RESET} [{large_image}]
-[{Fore.YELLOW}>{Fore.RESET}] Small_image {Fore.RED}->{Fore.RESET} [{small_image}]
-[{Fore.YELLOW}>{Fore.RESET}] Large_text  {Fore.RED}->{Fore.RESET} [{large_text}]
-''')
 
-# Updating Rich Presence.
-richPresence.update(details=details,
-            state=state,
-            large_image=large_image,
-            small_image=small_image,
-            large_text=large_text,
-            start=time.time())
+        # <!------  Config ------ >
+        details = config.details
+        state = config.state
+        large_image = config.large_image
+        small_image = config.small_image
+        large_text = config.large_text
+        # <!------ /Config ------ >
 
-print(F"\n[{Fore.GREEN}+{Fore.RESET}] Rich Presence Running.")
+        # Display .
+        print(F'''
+        [{Fore.YELLOW}>{Fore.RESET}] Details     {Fore.RED}->{Fore.RESET} [{details}]
+        [{Fore.YELLOW}>{Fore.RESET}] State       {Fore.RED}->{Fore.RESET} [{state}]
+        [{Fore.YELLOW}>{Fore.RESET}] Large_image {Fore.RED}->{Fore.RESET} [{large_image}]
+        [{Fore.YELLOW}>{Fore.RESET}] Small_image {Fore.RED}->{Fore.RESET} [{small_image}]
+        [{Fore.YELLOW}>{Fore.RESET}] Large_text  {Fore.RED}->{Fore.RESET} [{large_text}]
+        ''')
+
+        # Updating Rich Presence.
+        richPresence.update(details=details,
+                    state=state,
+                    large_image=large_image,
+                    small_image=small_image,
+                    large_text=large_text,
+                    start=time.time())
+
+        print(F"\n[{Fore.GREEN}+{Fore.RESET}] Rich Presence Running.")
+        time.sleep(9*900)
+    except pypresence.exceptions.DiscordNotFound:
+        print("[+] Waiting for client to connect to discord. ")
+        time.sleep(3)
+        os.system('cls||clear')
 
 # Looping time sleep so the script keep on running.
-while 1:
-    time.sleep(1)
 
 # <!-- Null -->
